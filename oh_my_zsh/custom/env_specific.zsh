04aafce507ed31c51d42e863013c6f9798e362b2
darren <darren@web-bloke.com>
2015-08-03 20:02:22 +0100

    environment specific config

--- oh_my_zsh/custom/env_specific.zsh
+++ oh_my_zsh/custom/env_specific.zsh
@@ -1,7 +1,8 @@
-# key bindings - so see what the key outputs try shift+ctrl+v
-# only needed on windows at the moment...
 if [[ $WINDIR = 'C:\Windows' ]]
 then
+   # running on windows
+
+   # update key bindings to get home/end and arrow keys working properly
    bindkey "^[[1~" beginning-of-line   # home key
    bindkey "^[[4~" end-of-line         # end key
    bindkey "^[[3~" delete-char         # del key
@@ -11,4 +12,7 @@ then
    bindkey "^[Od" backward-word        # ctrl + left arrow
    bindkey "^H" backward-kill-word     # ctrl + backspace
    bindkey "^[[3^" kill-word           # ctrl + delete
+
+   # remove the trailing % following command execution - http://zsh.sourceforge.net/FAQ/zshfaq03.html#l40
+   unsetopt promptcr
 fi
